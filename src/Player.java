557ce0c92904e75945639e45f8ace26f7c0e66d9
benok511 <benokeeffe76@gmail.com>
2026-01-14 20:51:19 +0000

    added more of the player logic

--- src/Player.java
+++ src/Player.java
@@ -17,4 +17,32 @@ public class Player extends Character {
         String strInventory = Integer.toString(inventory.length());
         return "Player | "+ super.toString() + " Money:" + strMoney  + " Inventory:" + strInventory;
     }
+
+
+    public int getMoney(){
+        return money;
+    }
+
+    public void setMoney(int money){
+         if (money < 0){
+             throw new IllegalArgumentException("Money must be positive!");
+         }
+         this.money = money;
+    }
+
+    public void pickUp(Item item){
+        if(inventory.getWeight() + item.getWeight() > getStrength()){
+            throw new IllegalArgumentException("You are not strong enough to carry this item");
+        }
+        inventory.addItem(item);
+    }
+
+    public void dropItem(int index){
+        if(index < 0 || index > inventory.length()){
+            throw new IndexOutOfBoundsException("Not valid cell in inventory");
+        }
+        inventory.removeItem(index);
+    }
+
+
 }
